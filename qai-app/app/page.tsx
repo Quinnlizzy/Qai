8438e635474462161d27989e7149ca90c3db10d5
Quinnlizzy <quin27@hotmail.co.uk>
2024-03-11 22:01:22 +0000

    updated styling

--- qai-app/app/page.tsx
+++ qai-app/app/page.tsx
@@ -5,24 +5,22 @@ import { useChat } from 'ai/react';
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch" style={{borderLeft: '1px solid cyan', borderRight: '1px solid cyan'}}>
-    {messages.map(m => (
-      <div key={m.id} className="whitespace-pre-wrap">
-        {m.role === 'user' ? 'User: ' : 'AI: '}
-        {m.content}
-      </div>
-    ))}
-  
-
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.map(m => (                              
+        <div key={m.id} className="message whitespace-pre-wrap" style={{borderLeft: '0.5px solid rgba(100, 255, 255, 1)', paddingLeft: '30px', borderRight: '0.5px solid rgba(100, 255, 255, 1)', paddingRight: '30px'}}>
+          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.content}
+        </div>
+      ))}
  
       <form onSubmit={handleSubmit}>
       <input
-  className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+  className="fixed bottom-0 w-full mx-auto max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
   value={input}
   placeholder="Say something..."
   onChange={handleInputChange}
-  style={{backgroundColor: 'black', border: '1px solid light pink', boxShadow: '0 0 40px hot pink'}}
-        />
+  style={{backgroundColor: 'black', border: '0.5px solid pink', boxShadow: '0 0 20px hotpink'}}
+/>
       </form>
     </div>
   );
